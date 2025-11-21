aefceddc46b9d3109501e2cb7908a2e525ed9e96
aditya-130906 <pes2ug24cs034@pesuonline.onmicrosoft.com>
2025-11-21 21:23:07 +0530

    force redeploy after repo rename

--- react-node-mongodb-app/server/src/models/User.js
+++ react-node-mongodb-app/server/src/models/User.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
+        required: true, 
     },
     phone: {
         type: String,
