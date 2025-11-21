e88cbb444626499216a5b811f6fcbcf3134eeb02
aditya-130906 <pes2ug24cs034@pesuonline.onmicrosoft.com>
2025-11-21 08:53:09 +0530

    Update hotel seed data and API URL

--- react-node-mongodb-app/client/src/utils/api.js
+++ react-node-mongodb-app/client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://web-tech-785e.onrender.com/api';
 
 export const apiCall = async (endpoint, options = {}) => {
     const url = `${API_BASE_URL}${endpoint}`;
